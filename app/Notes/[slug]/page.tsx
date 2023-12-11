91e83b1561fe566c710c9d0309c0cc9fc732f71d
Vladyslav Bilokon <vlad2004bil@gmail.com>
2023-12-11 17:08:40 +0200

    Fix 'localstorage isn't find'

--- app/Notes/[slug]/page.tsx
+++ app/Notes/[slug]/page.tsx
@@ -3,45 +3,46 @@ import { useSearchParams } from "next/navigation";
 import styles from "./page.module.css";
 import { useEffect, useState, useRef } from "react";
 
+function FuncResize(elem: any, list: any, setList: any, index: number) {
+  const updatedList = [...list];
+  updatedList[index][2] = elem.value;
+  setList(updatedList);
+}
+
 export default function Page({ params }: { params: { slug: string } }) {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-  const index = parseInt(id ?? "0", 10);
+  if (typeof window !== "undefined") {
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
+    const index = parseInt(id ?? "0", 10);
 
-  const decodedSlug = decodeURIComponent(params.slug);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const [list, setList] = useState(() => {
-    const listValue = localStorage.getItem("ListInfo");
-    return listValue ? JSON.parse(listValue) : [];
-  });
+    const decodedSlug = decodeURIComponent(params.slug);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const [list, setList] = useState(() => {
+      const listValue = localStorage.getItem("ListInfo");
+      return listValue ? JSON.parse(listValue) : [];
+    });
 
-  useEffect(() => {
-    localStorage.setItem("ListInfo", JSON.stringify(list));
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto"
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
-    }
-  }, [list]);
+    useEffect(() => {
+      localStorage.setItem("ListInfo", JSON.stringify(list));
+      if (textareaRef.current) {
+        textareaRef.current.style.height = "auto";
+        textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+      }
+    }, [list]);
 
-
-  function FuncResize(elem: any) {
-    const updatedList = [...list];
-    updatedList[index][2] = elem.value;
-    setList(updatedList);
-  }
-
-  return (
-    <div className={styles.main}>
-      <h2>{decodedSlug} :</h2>
-      <div className={styles.note_content}>
-        <textarea
-          ref={textareaRef}
-          value={list[index][2]}
-          onChange={(e) => FuncResize(e.target)}
-          placeholder="Type something ..."
-          spellCheck = "false"
-        />
+    return (
+      <div className={styles.main}>
+        <h2>{decodedSlug} :</h2>
+        <div className={styles.note_content}>
+          <textarea
+            ref={textareaRef}
+            value={list[index][2]}
+            onChange={(e) => FuncResize(e.target, list, setList, index)}
+            placeholder="Type something ..."
+            spellCheck="false"
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,44 +4,45 @@ import ListNote from "./Components/ListNote/ListNode";
 import AddFunction from "./Components/AddFunction/AddFunction";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [count, setCount] = useState(() => {
-    const countValue = localStorage.getItem("NoteCount");
-    return countValue ? JSON.parse(countValue) : 0;
-  });
+export default function Page() {
+  if (typeof window !== "undefined") {
+    const [count, setCount] = useState(() => {
+      const countValue = localStorage.getItem("NoteCount");
+      return countValue ? JSON.parse(countValue) : 0;
+    });
 
-  const [list, setList] = useState(() => {
-    const listValue = localStorage.getItem("ListInfo");
-    return listValue ? JSON.parse(listValue) : [];
-  });
+    const [list, setList] = useState(() => {
+      const listValue = localStorage.getItem("ListInfo");
+      return listValue ? JSON.parse(listValue) : [];
+    });
+    const detective = {
+      display: list.length === 0 ? "flex" : "none",
+    };
 
-  const detective = {
-    display: list.length === 0 ? "flex" : "none",
-  };
+    useEffect(() => {
+      localStorage.setItem("NoteCount", JSON.stringify(count));
+      localStorage.setItem("ListInfo", JSON.stringify(list));
+    }, [count, list]);
 
-  useEffect(() => {
-    localStorage.setItem("NoteCount", JSON.stringify(count));
-    localStorage.setItem("ListInfo", JSON.stringify(list));
-  }, [count, list]);
+    return (
+      <main className={styles.main}>
+        <div className={styles.detective} style={detective}>
+          <p>Empty ...</p>
+        </div>
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.detective} style={detective}>
-        <p>Empty ...</p>
-      </div>
-
-      <ListNote
-        list={list}
-        setList={setList}
-        count={count}
-        setCount={setCount}
-      />
-      <AddFunction
-        list={list}
-        setList={setList}
-        count={count}
-        setCount={setCount}
-      />
-    </main>
-  );
+        <ListNote
+          list={list}
+          setList={setList}
+          count={count}
+          setCount={setCount}
+        />
+        <AddFunction
+          list={list}
+          setList={setList}
+          count={count}
+          setCount={setCount}
+        />
+      </main>
+    );
+  }
 }
