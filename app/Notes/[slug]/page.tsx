2fe2d4ccc05393133d86d52c859036f7c24f75ec
Vladyslav Bilokon <vlad2004bil@gmail.com>
2023-12-11 22:42:23 +0200

    Fix textarea bug

--- app/Notes/[slug]/page.tsx
+++ app/Notes/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { useSearchParams } from "next/navigation";
 import styles from "./page.module.css";
 import { useEffect, useState, useRef } from "react";
 
-
 export default function Page({ params }: { params: { slug: string } }) {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
@@ -12,29 +11,31 @@ export default function Page({ params }: { params: { slug: string } }) {
   const decodedSlug = decodeURIComponent(params.slug);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [list, setList] = useState(() => {
-    try{
+    try {
       const listValue = localStorage.getItem("ListInfo");
       return listValue ? JSON.parse(listValue) : [];
-    }catch(error){}
+    } catch (error) {
+      console.log("The data isn't loaded");
+      return [];
+    }
   });
-
 
   useEffect(() => {
     if (textareaRef.current) {
       textareaRef.current.style.height = "auto";
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
-  }, []);
-
+  }, [list]);
 
   function FuncResize(elem: any) {
-    const updatedList = [...list];
-    updatedList[index][2] = elem.value;
-    setList(updatedList);
-    localStorage.setItem("ListInfo", JSON.stringify(list));
+    // Check if list[index] is defined before accessing its properties
+    if (list[index]) {
+      const updatedList = [...list];
+      updatedList[index][2] = elem.value;
+      setList(updatedList);
+      localStorage.setItem("ListInfo", JSON.stringify(updatedList));
+    }
   }
-
-
 
   return (
     <div className={styles.main}>
@@ -42,7 +43,7 @@ export default function Page({ params }: { params: { slug: string } }) {
       <div className={styles.note_content}>
         <textarea
           ref={textareaRef}
-          value={list[index][2]}
+          value={list[index]?.[2] || ""} // Use optional chaining to handle potential undefined
           onChange={(e) => FuncResize(e.target)}
           placeholder="Type something ..."
           spellCheck="false"
